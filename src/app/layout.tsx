e3563e74d530ff9bd27b93981fa16634eaf22134
gzych98 <111496194+gzych98@users.noreply.github.com>
2024-01-03 20:02:06 +0100

    aaaaa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next'
-import '../styles/globals.css'
 import '../styles/reset.css'
+import '../styles/globals.css'
+import { Open_Sans } from 'next/font/google'
+
+const openSans = Open_Sans({ subsets: ['latin'], weight: ['300', '400', '500', '600', '700'] })
 
 export const metadata: Metadata = {
   title: 'Akademia Debat Profilaktycznych',
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
     <main>
-      <h1>Akademia Debat Profilaktycznych</h1>
-      <h2>Taki fajny projekt</h2>
-      <p>Piękna stropna dla mojej pięknej dziewczyny</p>
+      <h1>h1 - Akademia Debat Profilaktycznych</h1>
+      <h2>h2 - Taki fajny projekt</h2>
+      <p>p - Piękna stropna dla mojej pięknej dziewczyny</p>
     </main>
   )
 }
